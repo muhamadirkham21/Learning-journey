e300d028c45913ae35453ca74468d5ad362435a8
irkham <muhirkham21@students.unnes.ac.id>
2023-08-08 23:14:14 +0700

    add some python file in automation

---
+++ learn-python/ADVANCED/AUTOMATION/socketLibrary.py
@@ -0,0 +1,16 @@
+
+# create simple web browser
+import socket 
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysocket.send(cmd)
+
+while True:
+    data = mysocket.recv(512)
+    if len(data) < 1 :
+        break
+    print(data.decode())
+
+mysocket.close()
